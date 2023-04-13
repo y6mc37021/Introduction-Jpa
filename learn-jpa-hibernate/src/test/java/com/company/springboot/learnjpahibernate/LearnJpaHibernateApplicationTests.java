4590cafc25757eba7d14b98148b65f75044c299a
NAVYA <NAVYA@LAPTOP-OVSI5NT1.station>
2023-04-13 02:41:43 +0100

    In Memory H2 DB Console

---
+++ learn-jpa-hibernate/src/test/java/com/company/springboot/learnjpahibernate/LearnJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.springboot.learnjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
